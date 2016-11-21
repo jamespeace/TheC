684ddae4a2437654e3328c1790185163277b2cc1
james <james.happylove@gmail.com>
2016-11-21 19:40:36 +0800

    delete the exe.

---
+++ chap1/1-19.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+void reverse(char *);
+int length(char *);
+
+int main(void)
+{
+	char s[10] = {"Hello"};
+	reverse(s);
+	printf("%s\n", s);
+
+	return 0;
+}
+
+void reverse(char *s)
+{
+	int i, j;
+	char temp;
+	i = 0;
+	j = length(s) - 1;
+
+	while(i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+int length(char *s)
+{
+	int i;
+	i = 0;
+	
+	while(s[i++] != '\0');
+	
+	return --i;
+
+		
+}
--- chap1/1-22.c
+++ chap1/1-22.c
@@ -1,72 +1,43 @@
+/* use two arrays to deal with the problem.
+ * next (a basic unit of the FOLDLENGTH)is the key of this program.
+ * 將 foldength視為一個處理單位，若是一個單位內全無空格，視為處理完。
+ * 若有空格，則next = location - spaceholder.表示已經處理的字元數目，
+ * 等到next 滿了，在處理一次
+ * 2016/10/19 15:50  手痛之下終於解決這個問題！
+ * */
 #include<stdio.h>
-#define MAX 1000
+#define MAX 100
 
-char line[MAX + 1]; 
+typedef struct _node *pnode;
 
-int getlines(void);
+struct _node{
+	char value[MAX];
+	pnode next;
+}node;
 
+pnode head, tail;
+
+void
 int main(void)
 {
-	int len, t, next;			 /* t == line's position.
-								  * next == a basic unit of
-								  * 		FOLDLENGTH.  */
-	int location, spaceholder;	 /* location == arr's position.
-								  * spaceholder == the position
-								  * of space in the arr.  */
-	const int FOLDLENGTH = 7; /* The max length of a line */
-	char arr[FOLDLENGTH + 2];
-	while((len = getlines()) > 0) {
-		if(len - 1 <= FOLDLENGTH) {
-			printf("%s", line);
-		}
-
-		else {
-				/* if this is an extra long line then we
-				 * loop through it replacing a space nearest
-				 * to the foldarea with a newline.
-				 * 			*/
-			t = 0;
-			location = 0;
-			next = 1;
-			spaceholder = 0;
-			while(t < len) {
-				arr[location] = line[t];
-				if(line[t] == ' ')
-					spaceholder = location;
-				if(next == FOLDLENGTH) {
-					if(spaceholder == 0) {
-						arr[++location] = '\n';
-						next = 0;
-					}else {
-						arr[spaceholder] = '\n';
-						next = location - spaceholder;
-					}
-					arr[++location] = '\0';
-					printf("%s", arr);
-					location = -1;
-					spaceholder = 0;
-				}
-				location++;
-				t++;
-				next++;
+	int n, i, p;
+	char s[MAX];
+	n = i = p = 0;
+	printf("The length of one line ?");
+	scanf("%d", &n);
+	
+	if(n < MAX)
+		while((c=getchar()) != EOF || c != '\n') 
+		{
+			while(i < n) 
+			{
+				if(c == '\t' || c == ' ')				
 			}
-			arr[location] = '\0';
-			printf("%s", arr);
 		}
-	}
+	else
+		printf("ERROR!the length is too long");
+
 	return 0;
 }
 
-int getlines(void)
-{
-	int i;
-	char c;
-	i = 0;
-	while((c = getchar()) != EOF && c != '\n')
-	{
-		line[i++] = c;	
-	}
-	line[i++] = '\n';
-	line[i] = '\0';
-	return i;
-}
+
